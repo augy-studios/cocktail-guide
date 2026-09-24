0aa7a00611a8da34ce7739185631983d9c528f39
Augy <ateoks@gmail.com>
2026-09-24 16:02:37 +0800

    init struct

--- main-site/sw.js
+++ main-site/sw.js
@@ -1,12 +1,12 @@
-const CACHE = "template-offline-v1";
+const CACHE = "cocktailguide-v1";
 
 const ASSETS = [
   "/",
   "/index.html",
   "/style.css",
   "/script.js",
-  "/templateicon1-192.png",
-  "/templateicon1-512.png",
+  "/XCG-192.png",
+  "/XCG-512.png",
   "/favicon.ico",
   "/manifest.json"
 ];
